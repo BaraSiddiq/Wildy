1ee4740176fb4a49375fdaa1105075d0b78d593d
Bara Siddiq <36809832+BaraSiddiq@users.noreply.github.com>
2019-06-07 10:15:38 +0300

    added internet permisson. created intent that goes to Nintendo Switch games.

--- app/src/main/java/com/example/android/wildy/MainActivity.java
+++ app/src/main/java/com/example/android/wildy/MainActivity.java
@@ -1,14 +1,34 @@
 package com.example.android.wildy;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView txt;
+    Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txt = findViewById(R.id.txt);
+        btn = findViewById(R.id.ok_btn);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String nintendoSwitchGamesUrl = "https://www.nintendo.com/games/switch/";
+                Intent nintendoIntent = new Intent(Intent.ACTION_VIEW);
+                nintendoIntent.setData(Uri.parse(nintendoSwitchGamesUrl));
+                startActivity(nintendoIntent);
+            }
+        });
     }
 }
